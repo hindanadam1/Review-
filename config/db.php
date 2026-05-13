a2177efe1a3c0cf5949ab084f9b8c141a080a83f
eyegue <e.eyegue@ecole-ipssi.net>
2026-05-13 22:42:48 +0200

    Add initial files and updates

--- config/db.php
+++ config/db.php
@@ -23,18 +23,47 @@ use App\Services\NewsletterService;
 use App\Services\ReviewService;
 use App\Services\UserService;
 
-$database = Database::getInstance([
-    'host' => 'localhost',
-    'dbname' => 'revieweo',
-    'charset' => 'utf8',
-    'username' => 'root',
-    'password' => 'hind',
-]);
+$databaseConfig = [
+    'host' => getenv('DB_HOST') ?: 'localhost',
+    'dbname' => getenv('DB_NAME') ?: 'revieweo',
+    'charset' => getenv('DB_CHARSET') ?: 'utf8',
+    'username' => getenv('DB_USER') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: 'hind',
+];
 
-$pdo = $database->getConnection();
-$authService = new AuthService($pdo);
-$reviewService = new ReviewService($pdo);
-$likeService = new LikeService($pdo);
-$userService = new UserService($pdo);
-$categoryService = new CategoryService($pdo);
+try {
+    $database = Database::getInstance($databaseConfig);
+    $pdo = $database->getConnection();
+    $authService = new AuthService($pdo);
+    $reviewService = new ReviewService($pdo);
+    $likeService = new LikeService($pdo);
+    $userService = new UserService($pdo);
+    $categoryService = new CategoryService($pdo);
+} catch (\PDOException $exception) {
+    $pdo = null;
+    $authService = null;
+    $reviewService = null;
+    $likeService = null;
+    $userService = null;
+    $categoryService = new class {
+        public function getAll(): array
+        {
+            return [
+                ['nom' => 'Drama'],
+                ['nom' => 'Comedy'],
+                ['nom' => 'Action'],
+                ['nom' => 'Horror'],
+                ['nom' => 'Sci-Fi'],
+                ['nom' => 'Western'],
+                ['nom' => 'Romance'],
+                ['nom' => 'Thriller'],
+                ['nom' => 'Fantasy'],
+                ['nom' => 'Apocalypse'],
+                ['nom' => 'Martial Arts'],
+                ['nom' => 'Sports'],
+            ];
+        }
+    };
+}
+
 $newsletterService = new NewsletterService();
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+chdir(__DIR__ . '/pages');
+require __DIR__ . '/pages/index.php';
